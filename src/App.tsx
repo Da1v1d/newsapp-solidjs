aa7fbc890b664a2b913d3a308e6d0a49dd11f9b5
David Adamyan <davoadamyan64@gmail.com>
2024-03-29 16:33:01 +0400

    yoooo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import type { Component } from "solid-js";
 import styles from "./App.module.css";
 
 const App: Component = () => {
-  return <div class={styles.App}>well hellorqrqwrqwrqw</div>;
+  return <div class={styles.App}>aaa aaaaa</div>;
 };
 
 export default App;
